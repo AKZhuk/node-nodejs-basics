292ed9376b3ec8accba9fde35cf298f0f49024bb
Aliaksei Zhuk <aliaksei_zhuk@epam.com>
2022-06-06 00:21:01 +0400

    feat: implement rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,14 @@
-export const rename = async () => {
-    // Write your code here 
+import fs from 'fs';
+
+const checkFiles = () => {
+  if (!fs.existsSync('files/wrongFilename.txt') || fs.existsSync('files/properFilename.md'))
+    throw new Error('FS operation failed');
 };
+
+export const rename = async () => {
+  checkFiles();
+  fs.rename('files/wrongFilename.txt', 'properFilename.md', (err) => {
+    if (err) throw new Error('FS operation failed');
+  });
+};
+rename();
