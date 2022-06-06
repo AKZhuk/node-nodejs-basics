e29338dc22b69bbcd0239c6a305c7c30ad1057a7
Aliaksei Zhuk <aliaksei_zhuk@epam.com>
2022-06-06 10:40:01 +0400

    feat: implement parse arguments function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,14 @@
 export const parseArgs = () => {
-    // Write your code here 
+  const args = [];
+  const temp = process.argv.slice(2);
+
+  for (let i in temp) {
+    temp[i].startsWith('--') ? args.push(temp[i]) : (args[args.length - 1] += ` is ${temp[i]}`);
+  }
+
+  args.forEach((element) => {
+    console.log(element);
+  });
 };
+
+parseArgs();
