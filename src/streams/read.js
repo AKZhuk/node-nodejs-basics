4b7b4ddc72c3d16129aa024b294ea99cac49e07b
Aliaksei Zhuk <aliaksei_zhuk@epam.com>
2022-06-06 13:46:20 +0400

    feat: implement streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,10 @@
+import fs from 'fs';
 export const read = async () => {
-    // Write your code here 
+  const readableStream = fs.createReadStream('./src/streams/files/fileToRead.txt', 'utf8');
+
+  readableStream.on('data', (data) => {
+    process.stdout.write(data);
+  });
 };
+
+read();
