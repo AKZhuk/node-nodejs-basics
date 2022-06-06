6a5740049106b320b32d5fb1abf4fd6aea3bb731
Aliaksei Zhuk <aliaksei_zhuk@epam.com>
2022-06-06 10:12:07 +0400

    feat: implement parse function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,10 @@
+process.env.RSS_name1 = 'value1';
+process.env.RSS_name2 = 'value2';
+process.env.RSS_name3 = 'value3';
+
 export const parseEnv = () => {
-    // Write your code here 
+  const variables = Object.entries(process.env).filter((envName) => envName[0].startsWith('RSS'));
+  variables.forEach((variable) => console.log(`${variable[0]}=${variable[1]}`));
 };
+
+parseEnv();
