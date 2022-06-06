720d7ccf227b0337947f61a3a5d0765717a3f066
Aliaksei Zhuk <aliaksei_zhuk@epam.com>
2022-06-06 14:19:03 +0400

    feat: implement child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,18 @@
+import child_process from 'child_process';
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = child_process.fork('./src/cp/files/script.js', args);
+
+  child.on('message', (data) => {
+    process.stdout.write(data);
+  });
+
+  child.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+
+  process.stdin.on('data', (data) => {
+    forkProcess.send(data);
+  });
 };
+
+spawnChildProcess([1, 2, 3]);
